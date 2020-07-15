6f646aa25f51581ab72435b048f0b3ca8c54fd0d
Alajiki Temilola <alajikitemilola@gmail.com>
2020-07-15 22:16:34 +0100

    Updated credentials for Heroku Database

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'shayo');
-define('DB_PASSWORD', 'password123456');
-define('DB_NAME', 'test');
+define('DB_SERVER', 'ec2-54-234-28-165.compute-1.amazonaws.com');
+define('DB_USERNAME', 'xpwuybqcaujdal');
+define('DB_PASSWORD', 'c4050b541402c7e73b23cb00cb6cb5e4d5268d19d03ed7367bdb962a05bb490f');
+define('DB_NAME', 'd4tucp28q59016');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
