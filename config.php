aa40de298235e90f6b772577b2beca5cc0378b1b
Alajiki Temilola <alajikitemilola@gmail.com>
2020-07-15 23:48:20 +0100

    Server Config

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'ec2-54-234-28-165.compute-1.amazonaws.com');
-define('DB_USERNAME', 'xpwuybqcaujdal');
-define('DB_PASSWORD', 'c4050b541402c7e73b23cb00cb6cb5e4d5268d19d03ed7367bdb962a05bb490f');
-define('DB_NAME', 'd4tucp28q59016');
+define('DB_SERVER', 'https://www.exitussolution.com.ng:2083/');
+define('DB_USERNAME', 'exituss1_sayo');
+define('DB_PASSWORD', 'p@ssword123456');
+define('DB_NAME', 'exituss1_test');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
