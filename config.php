fbe19dec4602fcfb64cef276755e6af0967fdf39
Alajiki Temilola <alajikitemilola@gmail.com>
2020-07-19 16:04:44 +0100

    Update the heroku configuration

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'shayo');
-define('DB_PASSWORD', 'password123456');
-define('DB_NAME', 'test');
+define('DB_SERVER', 'us-cdbr-east-02.cleardb.com');
+define('DB_USERNAME', 'b67456d4bbec5d');
+define('DB_PASSWORD', 'bce318a9');
+define('DB_NAME', 'heroku_a4275d67572b90d');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
